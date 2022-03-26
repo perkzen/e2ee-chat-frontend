8a95d6d0e93c2f1783e25859cb46d7502a29475e
perkzen <perko.domen@gmail.com>
2022-03-26 22:28:41 +0100

    feat: sending messages

--- src/components/pages/Chat/Chat.tsx
+++ src/components/pages/Chat/Chat.tsx
@@ -1,22 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import classes from './Chat.module.scss';
 import { Box } from '@mui/material';
 import { ChatHeader, Input } from '../../ui';
-import { Message } from '../../../store/models/Chat';
 import ChatMessage from '../../ui/ChatMessage/ChatMessage';
 import { v4 } from 'uuid';
+import { useForm } from 'react-hook-form';
+import { useAppSelector } from '../../../store/app/hooks';
 
-const messages: Message[] = [
-  { text: 'test', senderId: 'dsadsa', conversationId: 'ddasdas' },
-  {
-    text: 'test',
-    senderId: 'dsadsa',
-    conversationId: 'ddasdas',
-  },
-  { text: 'test', senderId: 'dsadsa', conversationId: 'ddasdas' },
-];
+interface InputData {
+  text: string;
+}
+
+const defaultValues: InputData = {
+  text: '',
+};
 
 const Chat: FC = () => {
+  const [messages, setMessage] = useState([
+    { text: 'test', senderId: 'dsadsa', conversationId: 'ddasdas' },
+    {
+      text: 'test',
+      senderId: 'dsadsa',
+      conversationId: 'ddasdas',
+    },
+    { text: 'test', senderId: 'dsadsa', conversationId: 'ddasdas' },
+  ]);
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  const { register, reset, handleSubmit } = useForm<InputData>({
+    reValidateMode: 'onChange',
+    defaultValues,
+  });
+
+  const onSubmit = (data: InputData) => {
+    setMessage([
+      ...messages,
+      { senderId: user!.id, text: data.text, conversationId: 'dada' },
+    ]);
+    reset();
+  };
+
   return (
     <Box className={classes.Container}>
       <ChatHeader />
@@ -25,9 +49,16 @@ const Chat: FC = () => {
           <ChatMessage message={message} key={v4()} />
         ))}
       </Box>
-      <Box className={classes.InputContainer}>
-        <Input placeholder={'Type a message...'} className={classes.Input} />
-      </Box>
+      <form
+        className={classes.InputContainer}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+          placeholder={'Type a message...'}
+          className={classes.Input}
+          {...register('text')}
+        />
+      </form>
     </Box>
   );
 };
