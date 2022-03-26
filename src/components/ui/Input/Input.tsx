b25fe0372df9c8752d02d2c1522b7a3af8dec55b
perkzen <perko.domen@gmail.com>
2022-03-26 12:04:13 +0100

    fix: input forwardRef

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,19 +1,22 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 import { BaseTextFieldProps, TextField } from '@mui/material';
 
 interface InputProps extends BaseTextFieldProps {
   errorText?: string;
 }
 
-const Input: FC<InputProps> = ({ errorText, ...props }) => {
-  return (
-    <TextField
-      error={errorText !== undefined}
-      helperText={errorText}
-      variant={'standard'}
-      {...props}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ errorText, ...props }, ref) => {
+    return (
+      <TextField
+        error={errorText !== undefined}
+        helperText={errorText}
+        variant={'outlined'}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Input;
