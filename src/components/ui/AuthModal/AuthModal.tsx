1aa0f4765a2301d6ff4165ce56e9c9cea16e4594
perkzen <perko.domen@gmail.com>
2022-03-26 12:12:03 +0100

    feat: add modal open state

--- src/components/ui/AuthModal/AuthModal.tsx
+++ src/components/ui/AuthModal/AuthModal.tsx
@@ -1,46 +1,45 @@
-import React, {FC, SyntheticEvent, useState} from 'react';
-import {Box, Dialog, DialogContent, DialogTitle} from "@mui/material";
-import {TabContext} from "@mui/lab";
-import TabList from "@mui/lab/TabList";
-import Tab from "@mui/material/Tab";
-import TabPanel from "@mui/lab/TabPanel";
-import {LoginTab, RegisterTab} from "../index";
+import React, { FC, SyntheticEvent, useState } from 'react';
+import { Box, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { TabContext } from '@mui/lab';
+import TabList from '@mui/lab/TabList';
+import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
+import { LoginTab, RegisterTab } from '../index';
 
 interface ModalProps {
-    open: boolean
-    onClose: () => void
+  open: boolean;
+  onClose: () => void;
 }
 
-const AuthModal: FC<ModalProps> = ({open, onClose}) => {
-    const [value, setValue] = useState('1');
+const AuthModal: FC<ModalProps> = ({ open, onClose }) => {
+  const [value, setValue] = useState('1');
 
-    const handleChange = (event: SyntheticEvent, newValue: string) => {
-        setValue(newValue);
-    };
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
-
-    return (
-        <TabContext value={value}>
-            <Dialog open={open} onClose={onClose} fullWidth>
-                <DialogTitle>
-                    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                        <TabList onChange={handleChange} variant={"fullWidth"}>
-                            <Tab label="Sign in" value="1"/>
-                            <Tab label="Sign up" value="2"/>
-                        </TabList>
-                    </Box>
-                </DialogTitle>
-                <DialogContent>
-                    <TabPanel value="1">
-                        <LoginTab/>
-                    </TabPanel>
-                    <TabPanel value="2">
-                        <RegisterTab/>
-                    </TabPanel>
-                </DialogContent>
-            </Dialog>
-        </TabContext>
-    );
+  return (
+    <TabContext value={value}>
+      <Dialog open={open} onClose={onClose} onBackdropClick={onClose} fullWidth>
+        <DialogTitle>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} variant={'fullWidth'}>
+              <Tab label="Sign in" value="1" />
+              <Tab label="Sign up" value="2" />
+            </TabList>
+          </Box>
+        </DialogTitle>
+        <DialogContent>
+          <TabPanel value="1">
+            <LoginTab />
+          </TabPanel>
+          <TabPanel value="2">
+            <RegisterTab />
+          </TabPanel>
+        </DialogContent>
+      </Dialog>
+    </TabContext>
+  );
 };
 
 export default AuthModal;
--- src/components/ui/Menu/Menu.tsx
+++ src/components/ui/Menu/Menu.tsx
@@ -1,25 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import classes from './Menu.module.scss';
 import { Box, Card, CardContent, Typography } from '@mui/material';
-import {AuthModal, Button} from '../index';
+import { AuthModal, Button } from '../index';
 
 const Menu: FC = () => {
-  return (
-      <>
-        <Card className={classes.Container}>
-          <CardContent>
-            <Typography variant="h4" align="center">
-              Chat
-            </Typography>
-            <Box className={classes.OnlineUsers}>Online users</Box>
-            <div className={classes.ButtonContainer}>
-              <Button text={'Login'} onClick={() => console.log('log')} />
-            </div>
-          </CardContent>
-        </Card>
-        <AuthModal open={true} onClose={()=>console.log("da")}/>
-      </>
+  const [modalOpen, setModalOpen] = useState(false);
 
+  const handleClose = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  return (
+    <>
+      <Card className={classes.Container}>
+        <CardContent>
+          <Typography variant="h4" align="center">
+            Chat
+          </Typography>
+          <Box className={classes.OnlineUsers}>Online users</Box>
+          <div className={classes.ButtonContainer}>
+            <Button text={'Login'} onClick={handleClose} />
+          </div>
+        </CardContent>
+      </Card>
+      <AuthModal open={modalOpen} onClose={handleClose} />
+    </>
   );
 };
 
