ad8eb66f9dac947be1b339d59500f3cf2efd372a
perkzen <perko.domen@gmail.com>
2022-03-26 20:26:13 +0100

    feat: add logout

--- src/components/ui/MenuHeader/MenuHeader.tsx
+++ src/components/ui/MenuHeader/MenuHeader.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
 import classes from './MenuHeader.module.scss';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, IconButton, Typography } from '@mui/material';
 import { createAvatar } from '../../../utils/createAvatar';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 interface MenuHeaderProps {
   username: string;
@@ -12,6 +13,9 @@ const MenuHeader: FC<MenuHeaderProps> = ({ username }) => {
     <Box className={classes.Container}>
       <Avatar src={createAvatar(username)} sx={{ width: 50, height: 50 }} />
       <Typography>{username}</Typography>
+      <IconButton className={classes.Icon}>
+        <LogoutIcon />
+      </IconButton>
     </Box>
   );
 };
